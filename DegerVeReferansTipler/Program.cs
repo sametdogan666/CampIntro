61cf2a4cc952d715b768872168154784da41f165
sametdoqann <sametdoqann-s.doqann1570@gmail.com>
2021-01-17 00:36:14 +0300

    Add project files.

---
+++ DegerVeReferansTipler/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace DegerVeReferansTipler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* 
+               int, decimal, float, double, bool = deger tip --> stack
+               array, class, interface = referans tip ---> stack + heap
+            */
+            int sayi1 = 10;
+            int sayi2 = 30;
+            sayi1 = sayi2; // sayi1'in değeri eşittir sayi2'nin değeri.
+            sayi2 = 65;
+            //sayi1 ?? 30
+
+            int[] sayilar1 = new int[] { 10, 20, 30 };
+            int[] sayilar2 = new int[] { 100, 200, 300 };
+            sayilar1 = sayilar2;
+            sayilar2[0] = 999;
+            //sayilar1[0] 999
+
+
+        }
+    }
+}
